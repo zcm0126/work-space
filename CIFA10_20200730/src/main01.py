261debcd15c05ea49e7823bae995de37edfc9943
zcm <541244514@qq.com>
2020-08-03 14:35:06 +0800

    1

--- CIFA10_20200730/src/main01.py
+++ CIFA10_20200730/src/main01.py
@@ -36,11 +36,11 @@ class Train:
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
-        self.dataset = datasets.CIFAR10("D:\datasets", train=True, download=True, transform=transform_train)
+        self.dataset = datasets.CIFAR10("./datasets", train=True, download=True, transform=transform_train)
         self.dataloader = DataLoader(self.dataset, batch_size=512, shuffle=True, drop_last=True)
 
         # 获取测试数据
-        self.test_dataset = datasets.CIFAR10("D:\datasets", train=False, download=True, transform=transform_train)
+        self.test_dataset = datasets.CIFAR10("./datasets", train=False, download=True, transform=transform_train)
         self.test_dataloader = DataLoader(self.test_dataset, batch_size=512, shuffle=True, drop_last=True)
 
         # 创建模型方式一
